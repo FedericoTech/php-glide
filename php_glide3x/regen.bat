25c76af86acc02eac61eae7654cb8a6a27ff579a
FedericoTech <david_rovrialta@hotmail.com>
2025-05-06 23:38:31 +0100

    more changes

---
+++ php_glide3x/regen.bat
@@ -0,0 +1,4 @@
+@set PHPRC=%CD%\php_release.ini
+@set PATH=%CD%\..\..\SDK\php-sdk-binary-tools\phpdev\vs16\x86\php-8.1.31-sources\Release_TS;%PATH%
+
+php ..\..\..\SDK\php-sdk-binary-tools\phpdev\vs16\x86\php-8.1.31-sources\build\gen_stub.php phpglide3x.stub.php
