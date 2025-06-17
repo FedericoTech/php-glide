7e1873a461ba7cc31526a957ff1d011c326ad632
FedericoTech <federico.roviralta@hotmail.com>
2025-06-18 00:28:41 +0100

    fog demo try

--- php_glide2x/demos/cube_aa_polygon.php
+++ php_glide2x/demos/cube_aa_polygon.php
@@ -11,22 +11,22 @@ $centre->x = 320;
 $centre->y = 240;
 
 $cubeVertices = [
-	//x,	y,		z,		r, 		g, 		b
-	[-1, 	-1, 	-1,		$color, 0,		0		],	//0
-    [ 1,	-1, 	-1,		0,		$color,	0		],	//1
-    [ 1,	 1, 	-1,		0,		0,		$color,	],	//2
-    [-1,	 1, 	-1,		$color,	0,		0		],	//3
-    [-1,	-1, 	 1,		0,		$color,	0		],	//4
-    [ 1,	-1, 	 1,		0,		0,		$color	],	//5
-    [ 1,	 1, 	 1,		$color,	0,		0		],	//6
-    [-1,	 1, 	 1,		0,		$color,	0		],	//7
+	//x,	y,		z,		r, 		g, 		b		a
+	[-1, 	-1, 	-1,		$color, 0,		0,		0.3],	//0
+    [ 1,	-1, 	-1,		0,		$color,	0,		0.3],	//1
+    [ 1,	 1, 	-1,		0,		0,		$color,	0.3],	//2
+    [-1,	 1, 	-1,		$color,	0,		0,		0.3],	//3
+    [-1,	-1, 	 1,		0,		$color,	0,		0.3],	//4
+    [ 1,	-1, 	 1,		0,		0,		$color,	0.3],	//5
+    [ 1,	 1, 	 1,		$color,	0,		0,		0.3],	//6
+    [-1,	 1, 	 1,		0,		$color,	0,		0.3],	//7
 ];
 
 $vertices = array_map(function($item){
 
 	$vertex = new GrVertex;
 
-	list($vertex->x, $vertex->y, $vertex->z, $vertex->r, $vertex->g, $vertex->b) = $item;
+	list($vertex->x, $vertex->y, $vertex->z, $vertex->r, $vertex->g, $vertex->b, $vertex->a) = $item;
 		
 	return $vertex;
 }, $cubeVertices);
@@ -52,6 +52,24 @@ grDepthMask(true);
 grCullMode( GrCullMode_t::GR_CULL_POSITIVE );
 
 
+$fogTable = [
+	0, 5, 10, 15, 19, 24, 28, 33,
+    37, 41, 45, 49, 53, 56, 60, 63,
+    67, 70, 73, 76, 79, 82, 85, 88,
+    91, 94, 96, 99, 101, 104, 106, 108,
+    111, 113, 115, 117, 119, 121, 123, 125,
+    127, 129, 131, 133, 134, 136, 138, 139,
+    141, 142, 144, 145, 147, 148, 150, 151,
+    153, 154, 156, 157, 158, 160, 161, 255
+];
+/*
+grFogColorValue(0x00FFFFFF);
+grFogTable($fogTable);
+grFogMode(GrFogMode_t::GR_FOG_WITH_TABLE);
+*/
+
+
+
 while (!_kbhit()) {
 	
 	grBufferClear( 0, 0, GrDepth_t::GR_WDEPTHVALUE_FARTHEST );
@@ -67,11 +85,13 @@ while (!_kbhit()) {
         $v = project($v, 1.0, 1.0, 3.0); // Basic projection
         $v->x = ($v->x + 1.0) * 320.0; // convert to screen
         $v->y = (1.0 - $v->y) * 240.0;
+		
+		//$v->oow = 1.0 / $v->z;
 		$v->flush();
 
 		$transformed[] = $v;
 	}
-	
+		
 	foreach($cubeIndices as $indices){
 		grAADrawPolygon(
 			count($indices),
